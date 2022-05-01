bf69f287ef8a7f079aabbce706cb0ec5d983d8a9
Nick Lennon <nick.lennon@hotmail.co.uk>
2022-05-01 21:09:27 +0100

    feat: add stylesheet with link and buttons to weapon display

---
+++ src/WeaponDisplay.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function WeaponDisplay () {
+    return (
+        <div className="container">
+            <h5>Click a weapon type to jump to it</h5>
+            <div>
+                <a className="waves-effect waves-light btn-large">Assault Rifles (AR)</a>
+                <a className="waves-effect waves-light btn-large">Submachine Guns (SMG)</a>
+                <a className="waves-effect waves-light btn-large">Light Machine Guns (LMG)</a>
+                <a className="waves-effect waves-light btn-large">Shotguns</a>
+                <a className="waves-effect waves-light btn-large">Precision Rifles</a>
+                <a className="waves-effect waves-light btn-large">Sniper Rifles</a>
+                <a className="waves-effect waves-light btn-large">Pistols</a>
+                <a className="waves-effect waves-light btn-large">Launchers</a>
+                <a className="waves-effect waves-light btn-large">Melee</a>
+            </div>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import ReactDOM from "react-dom/client"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 import About from "./About"
+import WeaponDisplay from "./WeaponDisplay"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render (
     <div>
         <Navbar />
-        <About />
+        <div className="content">
+            <About />
+            <WeaponDisplay />
+        </div>
         <Footer />
     </div>
 )
