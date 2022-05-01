7a7201cd25055d5d3a979153169bf4bb8551c8c5
Nick Lennon <nick.lennon@hotmail.co.uk>
2022-05-01 19:55:04 +0100

    feat: add Font Awesome and  Materialize links, added navbar component and imported to index.js

---
+++ src/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+export default function Navbar () {
+    return (
+        <div>
+            <nav>
+                <div className="nav-wrapper">
+                    <a href="#" className="brand-logo">Warzone Unlock Tracker</a>
+                    <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons"><i class="fa-solid fa-bars"></i></i></a>
+                    <ul className="right hide-on-med-and-down">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Challenges</a></li>
+                        <li><a href="#">Profile</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <ul className="sidenav" id="mobile-demo">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Challenges</a></li>
+                <li><a href="#">Profile</a></li>
+            </ul>
+        </div>
+    )
+}
